fbc758757e141a9cf5c218003a7f0c8c36486a5e
Samantha Getting <sgetting@comcast.net>
2018-02-13 13:37:26 -0600

    added comments

--- timing_generator.sv
+++ timing_generator.sv
@@ -3,8 +3,7 @@
 
 
 /*
-The active video begins the clock cycle after the fifo becomes not empty.s
-
+    DEIFNES based on the VGA timing standard. For this lab, we are using a 640x480 resolution at 25MHz with a 60Hz refresh rate
 */
 localparam H_UPPER_BOUND =800 - 1;
 localparam V_UPPER_BOUND =525*(H_UPPER_BOUND+1) - 1;
@@ -42,15 +41,20 @@ module timing_generator(
 );
 
 
-reg fifo_empty_db1, fifo_empty_db2;
+reg fifo_empty_db1;
 wire always_en = 1;
 
+//devounce the fifo_empty signal so we can look for a falling edge, on which we reset the counters
+//because our write clock to the FIFO is twice as fast as our read clock, we know that once there is data 
+//in the FIFO, it will never be empty again
 FF #(1) debounce1(.clk(clk), .rst(rst), .d(fifo_empty), .q(fifo_empty_db1), .en(always_en));
 wire FIFO_LOADED = ~fifo_empty & fifo_empty_db1;
 
-//counter register
+//counter registers
 reg [23:0] V_counter;
 reg [23:0] H_counter;
+
+//start the V counter at zero, and reset it when it reaches it's upper bound OR when the FIFO is loaded with data for the first time.
 always @(posedge clk, posedge rst) begin
 
     if(rst) V_counter <= 0;   
@@ -59,6 +63,9 @@ always @(posedge clk, posedge rst) begin
     else V_counter <= V_counter + 1;
 
 end
+
+
+//start the H counter at zero, and reset it when it reaches it's upper bound OR when the FIFO is loaded with data for the first time.
 always @(posedge clk, posedge rst) begin
 
     if(rst) H_counter <= 0;   
@@ -67,9 +74,16 @@ always @(posedge clk, posedge rst) begin
     else H_counter <= H_counter + 1;
 
 end
+
+//active video condition determines when color data will be sent and when the RBG lines will be held at zero
+//The active video begins the clock cycle after the fifo becomes not empty.
+
 assign active_video_condition =  (H_counter >= 0) && (V_counter >= 0) && (H_counter <= H_ACTIVE_VIDEO_COUNT) && (V_counter <= V_ACTIVE_VIDEO_COUNT);
 
+//constantly request data from the FIFO during an active video condition
 assign fifo_rreq = active_video_condition ? 1 : 0;
+
+//clock the data as we receive it from the FIFO
 reg [23:0] pixel_data;
 always @(posedge clk, posedge rst) begin
     if(rst) pixel_data <= 0;
@@ -77,7 +91,7 @@ always @(posedge clk, posedge rst) begin
     else if(!fifo_empty && active_video_condition) pixel_data <= fifo_data;
 end
 
-
+//HSYNC and VSYNC counters
 assign hsync = (H_counter >= 0) && (H_counter >= HSYNC_OFF_LOW_BOUND) && (H_counter < HSYNC_OFF_HIGH_BOUND) ? 0 : 1;
 assign vsync = (V_counter >= 0) && (V_counter > VSYNC_OFF_LOW_BOUND) && (V_counter < VSYNC_OFF_HIGH_BOUND) ? 0 : 1;
 
@@ -85,7 +99,10 @@ assign red = (active_video_condition) ? pixel_data[23:16] : 0;
 assign green = (active_video_condition) ? pixel_data[15:8] : 0;
 assign blue = (active_video_condition) ? pixel_data[7:0] : 0;
 
+//blank_n forces all the RGB values to zero. we want this whenever there is no active video condition
 assign blank_n = (H_counter >= 0) && ((H_counter > H_ACTIVE_VIDEO_COUNT) || (V_counter > V_ACTIVE_VIDEO_COUNT))? 0:1;
-assign sync_n = 1;//~(hsync ^ vsync);
+
+//sync_n forces the G line down to a special value (encoded sync signal)
+assign sync_n = (hsync ^ vsync);
 
 endmodule
--- timing_generator_tb.sv
+++ timing_generator_tb.sv
@@ -1,14 +1,25 @@
 
 
-module timing_generator_tb(output dummy);
+module timing_generator_tb(output dummy); //this output is here so quartus will not get mad when we compile it as the top level modiule
 
+//wires to connect to the DUT
     reg  fifo_empty;
-    reg [23:0]  fifo_data;
+    wire [23:0]  fifo_data;
     reg clk, rst;
     reg  fifo_rreq;
     wire [7:0]  red, green, blue;
     wire  hsync, vsync, sync_n, blank_n;
 
+
+
+
+//these memories hold the pixels recieved from the VGA (simulates a recieving video screen
+    reg [7:0] red_pixels_rec [(640*480 -1) : 0];
+    reg [7:0] blue_pixels_rec [(640*480 -1) : 0];
+    reg [7:0] green_pixels_rec [(640*480 -1) : 0];
+
+
+//task that waits one clock cycle. simple but makes the code easier to understand
 task wait_num_pixel;
     input [31:0] pixels;
     
@@ -18,13 +29,31 @@ task wait_num_pixel;
     end
 endtask
 
+
+//counting variables
+int i;
+int j;
+
+//monitoring the sync_n signal
 wire expected_sync_n;
 assign expected_sync_n = hsync ^ vsync;
 wire match_sync_n = (expected_sync_n) ^ sync_n ? 0 : 1;
+
+//this tells the simulator when to capture color data
 reg active_video;
 
-reg [23:0] screen [640][480];
+//this is a variable that the recieved color data is checked against
+reg [7:0] expected_val;
 
+//keeps track of how many frames we have simulated
+int frame;
+
+//this holds the color values for the pixel data. Because we are not simulating with a FIFO, we will increment this value 
+// and assign the R, G, B color lines to it's value as if it was data from the FIFO
+reg [7:0] red_blue_green_data_pointer;
+
+
+//Instantiation of our DUT
 timing_generator DUT(  
     .clk(clk),
     .rst(rst),
@@ -40,181 +69,202 @@ timing_generator DUT(
      .blank_n(blank_n)
 );
 
+
+
 initial begin
+    //initialze signals, pulse reset
+    //mimick the FIFO being loaded with a falling edge on fifo_empty
     clk = 0;
     rst = 1;
     fifo_empty = 1;
     active_video = 0;
+    red_blue_green_data_pointer = 0;
     @(posedge clk);
     fifo_empty = 0;
-    fifo_data = 24'h00000000;
     rst = 0;
+
+
+    //start active video so our simualted VGA reciever knows that color data is valid
+    active_video = 1;
 
     @(posedge clk);
         
-    active_video = 1;
     $monitor("Error on sync_n! at time %d", match_sync_n, $time);
     #1;
 
     
    //active video
-repeat(10) begin
-   fork
+    for (frame = 0; frame < 40; frame = frame + 1) begin
 
-    begin
-        repeat (480) begin
-            if(hsync != 1) $display("hsync incorrect. not 1 during active video at %d", $time/10);
-          //  else $display("hsync correct!  %d", $time/10);
-            wait_num_pixel(640);
-            if(hsync != 1) $display("hsync incorrect. not 1 during front porch at %d", $time/10);
-            // else $display("hsync correct! %d", $time/10);
-            wait_num_pixel(16);
-            if(hsync != 0) $display("hsync incorrect. not 0 during sync pulse %d", $time/10);
-            // else $display("hsync correct! %d", $time/10);
-            wait_num_pixel(96);
-            if(hsync != 1 ) $display("hsync incorrect. not 1 during back porch %d", $time/10);
-            // else $display("hsync correct! %d", $time/10);    
-            wait_num_pixel(48);
-           
-        end
-   end
+/*
 
-   begin
-        if(vsync != 1) $display("vsync incorrect. not data during active video at %d", $time/10);
-        else $display("vsync correct!  %d", $time/10);
-        wait_num_pixel(480*800);
-        if(vsync != 1) $display("vsync incorrect. not 0 during front porch at %d", $time/10);
-        else $display("vsync correct! %d", $time/10);
-        wait_num_pixel(10*800);
-        if(vsync != 0) $display("vsync incorrect. not 0 during sync pulse %d", $time/10);
-        else $display("vsync correct! %d", $time/10);
-        wait_num_pixel(2*800);
-        if(vsync != 1 ) $display("vsync incorrect. not 0 during back porch %d", $time/10);
-        else $display("vsync correct! %d", $time/10);    
-        wait_num_pixel(33*800);
-   end
+    For each frame we send, check the hsync/vsync/color signals simeotanouesly 
 
-   begin
-   repeat (480) begin
-        active_video = 1;
-        if(red != fifo_data[23:16]) $display("red incorrect. not data during active video at %d, is %x", $time/10, red);
-        //else $display("red correct!  %d", $time/10);
-        wait_num_pixel(640);
-        active_video = 0;
-        if(red != 0) $display("red incorrect. not 0 during front porch at %d, is %x", $time/10, red);
-        //else $display("red correct! %d", $time/10);
-        wait_num_pixel(16);
-        if(red != 0) $display("red incorrect. not 0 during sync pulse %d, is %x", $time/10, red);
-        //else $display("red correct! %d", $time/10);
-        wait_num_pixel(96);
-        if(red != 0 ) $display("red incorrect. not 0 during back porch %d, is %x", $time/10, red);
-        //else $display("red correct! %d", $time/10);
-        wait_num_pixel(48); 
-        end   
-   end
-   
-      begin
-      repeat (480) begin
+*/
 
-        if(blue != fifo_data[7:0]) $display("blue incorrect. not data during active video at %d, is %x", $time/10, blue);
-        //else $display("blue correct!  %d", $time/F0);
-        wait_num_pixel(640);
-        if(blue != 0) $display("blue incorrect. not 0 during front porch at %d, is %x", $time/10, blue);
-        //else $display("blue correct! %d", $time/10);
-        wait_num_pixel(16);
-        if(blue != 0) $display("blue incorrect. not 0 during sync pulse %d, is %x", $time/10, blue);
-        //else $display("blue correct! %d", $time/10);
-        wait_num_pixel(96);
-        if(blue != 0 ) $display("blue incorrect. not 0 during back porch %d, is %x", $time/10, blue);
-        //else $display("blue correct! %d", $time/10);
-        wait_num_pixel(48);
-        end 
-        end   
-           
-      begin
-      repeat (480) begin
-      
-        if(green != fifo_data[15:8]) $display("green incorrect. not 1 during active video at %d, is %d", $time/10, green);
-        //else $display("green correct!  %d", $time/10);
-        wait_num_pixel(640);
-        if(green != 0) $display("green incorrect. not 1 during front porch at %d, is %x", $time/10, green);
-        //else $display("green correct! %d", $time/10);
-        wait_num_pixel(16);
-        if(green != 0) $display("green incorrect. not 0 during sync pulse %d, is %x", $time/10, green);
-        //else $display("green correct! %d", $time/10);
-        wait_num_pixel(96);
-        if(green != 0 ) $display("green incorrect. not 1 during back porch %d, is %x", $time/10, green);
-        //else $display("green correct! %d", $time/10);
-        wait_num_pixel(48);
-        end  
-        end
+    fork
 
-    begin
-      repeat (480) begin
-      
-        if(blank_n != 1) $display("green incorrect. not 1 during active video at %d, is %d", $time/10, green);
-        //else $display("green correct!  %d", $time/10);
-        wait_num_pixel(640);
-        if(blank_n != 0) $display("green incorrect. not 1 during front porch at %d, is %x", $time/10, green);
-        //else $display("green correct! %d", $time/10);
-        wait_num_pixel(16);
-        if(blank_n != 0) $display("green incorrect. not 0 during sync pulse %d, is %x", $time/10, green);
-        //else $display("green correct! %d", $time/10);
-        wait_num_pixel(96);
-        if(blank_n != 0 ) $display("green incorrect. not 1 during back porch %d, is %x", $time/10, green);
-        //else $display("green correct! %d", $time/10);
-        wait_num_pixel(48);
-        end  
-        end
-
-    begin
-
-    end
-
-    begin
-
-        repeat (480*8000) @(negedge clk) begin
-             if(active_video)
-             fifo_data = fifo_data + 1;
-        end
-    end
-   join
-$display("complete");
-end
-end
-
-
-int i,j;
-int expected;
-initial begin
-    i = 0;
-    j = 0;
-    while(j < 480) begin
-        @(posedge clk);
-        #1;
-        if(active_video) begin
-            screen[i][j] = {red[7:0], green[7:0], blue[7:0]};
-            i = 1 + i;
-            if(i == 640) begin
-                i = 0;
-                j = 1 + j;
+        begin
+            repeat (480) begin  //we are sending 480 lines per frame
+                if(hsync != 1) $display("hsync incorrect. not 1 during active video at %d", $time/10);
+            //  else $display("hsync correct!  %d", $time/10);
+                wait_num_pixel(639);
+                active_video = 0;
+                wait_num_pixel(1);
+                if(hsync != 1) $display("hsync incorrect. not 1 during front porch at %d", $time/10);
+                // else $display("hsync correct! %d", $time/10);
+                wait_num_pixel(16);
+                if(hsync != 0) $display("hsync incorrect. not 0 during sync pulse %d", $time/10);
+                // else $display("hsync correct! %d", $time/10);
+                wait_num_pixel(96);
+                if(hsync != 1 ) $display("hsync incorrect. not 1 during back porch %d", $time/10);
+                // else $display("hsync correct! %d", $time/10);    
+                wait_num_pixel(48);
+            
             end
+    end
+
+    begin
+            if(vsync != 1) $display("vsync incorrect. not data during active video at %d", $time/10);
+            else $display("vsync correct!  %d", $time/10);
+            wait_num_pixel(480*800);
+            if(vsync != 1) $display("vsync incorrect. not 0 during front porch at %d", $time/10);
+            else $display("vsync correct! %d", $time/10);
+            wait_num_pixel(10*800);
+            if(vsync != 0) $display("vsync incorrect. not 0 during sync pulse %d", $time/10);
+            else $display("vsync correct! %d", $time/10);
+            wait_num_pixel(2*800);
+            if(vsync != 1 ) $display("vsync incorrect. not 0 during back porch %d", $time/10);
+            else $display("vsync correct! %d", $time/10);    
+            wait_num_pixel(33*800);
+    end 
+
+    begin
+    repeat (480) begin //we are sending 480 lines per frame
+            active_video = 1;
+            
+            if(red != fifo_data[23:16]) $display("red incorrect. not data during active video at %d, is %x,  should be %x", $time/10, red, fifo_data[23:16]);
+            //else $display("red correct!  %d", $time/10);
+            wait_num_pixel(640);
+            active_video = 0;
+            if(red != 0) $display("red incorrect. not 0 during front porch at %d, is %x", $time/10, red);
+            //else $display("red correct! %d", $time/10);
+   
+            wait_num_pixel(16);
+            if(red != 0) $display("red incorrect. not 0 during sync pulse %d, is %x", $time/10, red);
+            //else $display("red correct! %d", $time/10);
+            wait_num_pixel(96);
+            if(red != 0 ) $display("red incorrect. not 0 during back porch %d, is %x", $time/10, red);
+            //else $display("red correct! %d", $time/10);
+            wait_num_pixel(48); 
+            end   
+    end
+    
+        begin
+        repeat (480) begin //we are sending 480 lines per frame
+            #1
+            if(blue != fifo_data[7:0]) $display("blue incorrect. not data during active video at %d, is %x, should be %x", $time/10, blue, fifo_data[7:0]);
+            //else $display("blue correct!  %d", $time/F0);
+            wait_num_pixel(640);
+            if(blue != 0) $display("blue incorrect. not 0 during front porch at %d, is %x", $time/10, blue);
+            //else $display("blue correct! %d", $time/10);
+            wait_num_pixel(16);
+            if(blue != 0) $display("blue incorrect. not 0 during sync pulse %d, is %x", $time/10, blue);
+            //else $display("blue correct! %d", $time/10);
+            wait_num_pixel(96);
+            if(blue != 0 ) $display("blue incorrect. not 0 during back porch %d, is %x", $time/10, blue);
+            //else $display("blue correct! %d", $time/10);
+            wait_num_pixel(48);
+            end 
+            end   
+            
+        begin
+        repeat (480) begin //we are sending 480 lines per frame
+        
+            if(green != fifo_data[15:8]) $display("green incorrect. not 1 during active video at %d, is %x, should be %x", $time/10, green, fifo_data[15:8]);
+            //else $display("green correct!  %d", $time/10);
+            wait_num_pixel(640);
+            if(green != 0) $display("green incorrect. not 1 during front porch at %d, is %x", $time/10, green);
+            //else $display("green correct! %d", $time/10);
+            wait_num_pixel(16);
+            if(green != 0) $display("green incorrect. not 0 during sync pulse %d, is %x", $time/10, green);
+            //else $display("green correct! %d", $time/10);
+            wait_num_pixel(96);
+            if(green != 0 ) $display("green incorrect. not 1 during back porch %d, is %x", $time/10, green);
+            //else $display("green correct! %d", $time/10);
+            wait_num_pixel(48);
+            end  
+            end
+
+        begin
+        repeat (480) begin //we are sending 480 lines per frame
+        
+            if(blank_n != 1) $display("green incorrect. not 1 during active video at %d, is %d", $time/10, green);
+            //else $display("green correct!  %d", $time/10);
+            wait_num_pixel(640);
+            if(blank_n != 0) $display("green incorrect. not 1 during front porch at %d, is %x", $time/10, green);
+            //else $display("green correct! %d", $time/10);
+            wait_num_pixel(16);
+            if(blank_n != 0) $display("green incorrect. not 0 during sync pulse %d, is %x", $time/10, green);
+            //else $display("green correct! %d", $time/10);
+            wait_num_pixel(96);
+            if(blank_n != 0 ) $display("green incorrect. not 1 during back porch %d, is %x", $time/10, green);
+            //else $display("green correct! %d", $time/10);
+            wait_num_pixel(48);
+            end  
+            end
+
+    join
+
+//after the frame has been sent, check the recieved pixel data is what we expect it to be
+
+ expected_val = 0;
+    for(j=0; j < 640*480;j++) begin
+        if(red_pixels_rec[j] != expected_val) $display("error on red index %d", i);
+        if(blue_pixels_rec[j] != expected_val) $display("error on blue index %d", i);
+        if(green_pixels_rec[j] != expected_val) begin
+             $display("error on green index %d", i);
+            while(1) #1;
+        end
+      
+        expected_val++;
+
+        
+    end
+
+    $display("Finished checking one frame one frame. Frame %d has completed", frame);
+    end
+end
+
+
+//this always block acts as the FIFO, changing the pixel data every clock cycle
+// it changes the pixel data on the negedge clk to avoid simulation timing issues that arise from changing it on the posedge
+//to mimick FIFO data, we start the pixel data at 0 and incrememt it every clock cycle. This way we have an expected value for 
+//what the timing_generator should output without having to use large memories. 
+assign fifo_data = {red_blue_green_data_pointer, red_blue_green_data_pointer ,red_blue_green_data_pointer} ;
+always @(negedge clk) begin
+        
+        if(active_video) 
+        red_blue_green_data_pointer++;
+        
+end
+
+
+//this always block acts as the video reciever, taking in the pixel data and storing it until we finish sending the whole frame.
+initial begin
+repeat (39) begin
+//recives the pixels and stores them in *_pixels_recived
+    i = 0;
+    while(i < 480*640) begin
+        @(posedge clk);
+        if(active_video) begin
+            {red_pixels_rec[i], green_pixels_rec[i], blue_pixels_rec[i]} = {red[7:0], green[7:0], blue[7:0]};
+            i =     1 + i;
         end
     
     end
-    expected = 0;
-    for(j = 0; j < 480; j = 1 + j) begin
-        for(i = 0; i < 640; i = 1 + i) begin
-            if(screen[i][j] != expected) begin 
-                #1 
-                $display("error on screen index %d %d. Should be %d, is %d", i, j, expected, screen[i][j]);
-            end
-            expected = 1 + expected;
-            if(screen[i][j] == 0 && i !=0 ) while(1) #1;
+end
 
-         end
-
-    end
 end
 
 always #5 clk = ~clk;
